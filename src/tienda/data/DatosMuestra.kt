917e0bc80e65d16726bf0afcda9e1ec1cb93e071
eamguille <eamguille@gmail.com>
2025-09-24 19:30:41 -0600

    Agregando datos quemados para pruebas

---
+++ src/tienda/data/DatosMuestra.kt
@@ -0,0 +1,11 @@
+package tienda.data
+
+import tienda.modelos.Producto
+
+fun DatosMuestra() = listOf(
+    Producto(1, "Audifonos", 3.00, 22),
+    Producto(2, "Lapiceros", 0.25, 175),
+    Producto(3, "Taqueritos", 0.50, 200),
+    Producto(4, "CocaCola lata", 0.80, 120),
+    Producto(5, "Jugo del Valle", 0.75, 200),
+)
